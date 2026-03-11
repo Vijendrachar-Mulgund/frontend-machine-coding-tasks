ffb5eee6d3474a50f646ea51c64b849702701da4
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2026-03-11 23:36:13 +0530

    enhancements made to accordion implementation

--- accordion/index.js
+++ accordion/index.js
@@ -1,37 +1,40 @@
-const accordionItemElements = [];
+const accordionItemElements = {};
 const accordionRootElement = document.getElementById("accordion-root");
 
-const handleOnToggleClick = function (event) {
-  const accElement = event.target.parentNode;
-  const style = window.getComputedStyle(accElement.children[1]);
-  if (style.display == "none") {
-    accElement.children[1].style.display = "block";
-  } else {
-    accElement.children[1].style.display = "none";
+accordionRootElement.addEventListener("click", function (event) {
+  if (event.target.className == "accordion-delete-button") {
+    const accordionItemElement = event.target.closest(".accordion-item");
+    const idOfElement = accordionItemElement.id;
+    if (idOfElement) {
+      delete accordionItemElements[idOfElement];
+      accordionRootElement.removeChild(accordionItemElement);
+      return;
+    }
+    return;
   }
-};
+
+  const accordionItemElement = event.target.closest(".accordion-item");
+  const accordionContentElement = accordionItemElement.children[1]; // The Content is always the second node in the list
+  const accordionContentElementStyle = window.getComputedStyle(accordionContentElement);
+  if (accordionContentElementStyle.display == "none") {
+    accordionContentElement.style.display = "block";
+  } else {
+    accordionContentElement.style.display = "none";
+  }
+});
 
 const handleOnAddClick = function () {
-  const newAccordionElement = `<div id="acc-${accordionItemElements.length + 1}" class="accordion-item">
-        <div class="accordion-header">Header</div>
+  const newId = `acc-${Object.keys(accordionItemElements).length + 1}`;
+  const newAccordionElement = `
+    <div id="${newId}" class="accordion-item">
+        <div class="accordion-header-content">
+            <div class="accordion-header">Header</div>
+            <button class="accordion-delete-button">Del</button>
+        </div>
         <div class="accordion-content">Content</div>
       </div>`;
-  accordionItemElements.push(newAccordionElement);
-  return renderList();
-};
-
-const renderList = () => {
-  document.getElementById("accordion-root").innerHTML = accordionItemElements;
-  const accordionElementsDOM = document.querySelectorAll(".accordion-item");
-  accordionElementsDOM.forEach((accElement) => {
-    accElement.children[0].addEventListener("click", handleOnToggleClick);
-  });
+  accordionItemElements[newId] = newAccordionElement;
+  accordionRootElement.insertAdjacentHTML("beforeend", newAccordionElement);
 };
 
 document.getElementById("add-accordion").addEventListener("click", handleOnAddClick);
-document.getElementById("accordion-root").innerHTML = accordionItemElements;
-
-const accordionElementsDOM = document.querySelectorAll(".accordion-item");
-accordionElementsDOM.forEach((accElement) => {
-  accElement.children[0].addEventListener("click", handleOnToggleClick);
-});
