c8581fd66a5bba50a3a5e1a313489e02aa17b4e9
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2026-03-12 00:20:47 +0530

    addressed all the accordion review coments

--- accordion/index.js
+++ accordion/index.js
@@ -1,39 +1,41 @@
-const accordionItemElements = {};
 const accordionRootElement = document.getElementById("accordion-root");
 
 accordionRootElement.addEventListener("click", function (event) {
-  if (event.target.className == "accordion-delete-button") {
+  if (event.target.classList.contains("accordion-delete-button")) {
     const accordionItemElement = event.target.closest(".accordion-item");
     const idOfElement = accordionItemElement.id;
     if (idOfElement) {
-      delete accordionItemElements[idOfElement];
       accordionRootElement.removeChild(accordionItemElement);
-      return;
     }
     return;
   }
 
-  const accordionItemElement = event.target.closest(".accordion-item");
-  const accordionContentElement = accordionItemElement.children[1]; // The Content is always the second node in the list
-  const accordionContentElementStyle = window.getComputedStyle(accordionContentElement);
-  if (accordionContentElementStyle.display == "none") {
-    accordionContentElement.style.display = "block";
-  } else {
-    accordionContentElement.style.display = "none";
+  if (event.target.classList.contains("accordion-header")) {
+    const accordionItemElement = event.target.closest(".accordion-item");
+    const accordionHeaderElement = event.target.closest(".accordion-header");
+    const accordionContentElement = accordionItemElement.children[1]; // The Content is always the second node in the element
+
+    if (!accordionContentElement.classList.contains("open")) {
+      accordionContentElement.classList.add("open");
+      accordionHeaderElement.ariaExpanded = "true";
+    } else {
+      accordionContentElement.classList.remove("open");
+      accordionHeaderElement.ariaExpanded = "false";
+    }
   }
 });
 
 const handleOnAddClick = function () {
-  const newId = `acc-${Object.keys(accordionItemElements).length + 1}`;
+  const newId = `acc-${Date.now()}`;
+  console.log("newId", newId);
   const newAccordionElement = `
     <div id="${newId}" class="accordion-item">
         <div class="accordion-header-content">
-            <div class="accordion-header">Header</div>
+            <div aria-expanded="false" class="accordion-header">Header</div>
             <button class="accordion-delete-button">Del</button>
         </div>
         <div class="accordion-content">Content</div>
       </div>`;
-  accordionItemElements[newId] = newAccordionElement;
   accordionRootElement.insertAdjacentHTML("beforeend", newAccordionElement);
 };
 
